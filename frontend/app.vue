d32916cafa750a543cce9d6b08547822cb47d4e0
Filip Jagieła <filip.jagiela@tylko.com>
2026-05-10 18:46:04 +0200

    feat: inject Umami tracking script in Nuxt app

--- frontend/app.vue
+++ frontend/app.vue
@@ -2,3 +2,19 @@
   <NuxtRouteAnnouncer />
   <NuxtPage />
 </template>
+
+<script setup lang="ts">
+const config = useRuntimeConfig()
+
+if (config.public.umamiWebsiteId && config.public.umamiScriptUrl) {
+  useHead({
+    script: [
+      {
+        src: config.public.umamiScriptUrl,
+        defer: true,
+        'data-website-id': config.public.umamiWebsiteId,
+      },
+    ],
+  })
+}
+</script>
